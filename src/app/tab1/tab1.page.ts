84b9130d98402bd4e380d0fc9eadc9a3db95e5ff
bbosak1 <bbosak1@live.maryville.edu>
2020-07-24 22:41:44 -0500

    Change items to contacts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ToastController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,25 +9,68 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  items = [
+  title = "Tutor";
+
+  contacts = [
     {
-      name: "Donuts",
-      quantity: 12
+      name: "John Doe",
+      phoneNo: "555-123-4567"
     },
     {
-      name: "Bacon",
-      quantity: 3
-    },
-    {
-      name: "Sausage",
-      quantity: 2
-    },
-    {
-      name: "Eggs",
-      quantity: 12
+      name: "Jane Doe",
+      phoneNo: "555-999-0000"
     },
   ];
 
-  constructor() {}
+  constructor (
+    public navCtrl: NavController,
+    public toastCtrl: ToastController,
+    public alertCtrl: AlertController) {
 
+  }
+
+  removeContact(contact, i) {
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item - ' + contact.name + " ...",
+      duration: 2000
+    });
+    this.toastCtrl.create();
+    this.contacts.splice(i, 1);
+  }
+
+  addContact() {
+    this.showAddContactPrompt()
+  }
+
+  async showAddContactPrompt() {
+    const prompt = await this.alertCtrl.create({
+      header: 'Add Item',
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'phoneNo',
+          placeholder: 'Phone Number'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: contact => {
+            this.contacts.push(contact);
+          }
+        }
+      ]
+    });
+    await prompt.present();
+  }
 }
